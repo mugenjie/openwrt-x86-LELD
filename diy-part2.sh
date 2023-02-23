5eb31b2bc56ff42543b01be208ca3012470b9c20
mugenjie <85976222+mugenjie@users.noreply.github.com>
2023-02-23 13:42:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/15.4.8.100/15.8.50.5/g' package/base-files/files/bin/config_generate
