8a11ada3290b139abc4392164616287204375465
mugenjie <85976222+mugenjie@users.noreply.github.com>
2023-02-27 12:32:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-# echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-# echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-# echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-# echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-# echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
